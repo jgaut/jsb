7c8efaa9e943a473b0bc2a41b6f003cdf9dbb662
jgaut <gautierjeremy@gmail.com>
2019-02-06 11:49:30 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,43 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import ResponsiveContainer from "react-responsive-widget";
+
+import imageBck from "./images/photo-rubrique-1_0.jpg";
+
+var myStyle = {
+	backgroundImage: 'url(' + imageBck + ')',
+	backgroundSize: 'cover',
+        backgroundPosition: 'center center',
+        backgroundRepeat: 'no-repeat',
+	flex: 1,
+	flexDirection: 'row',
+	alignItems: 'strech',
+	justifyContent: 'center',
+	padding: '10px 10px 10px 10px'
+}
+
+var myStyleDiv = {
+
+	minWidth: 200
+
+}
+
+let myCol = "app-col-xs-12 app-col-sm-3 app-col-md-3 app-col-lg-3"
+//let myCol = "app-col"
 
 class App extends Component {
-  render() {
+
+ render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+<ResponsiveContainer>
+                <div className="app-row" style={myStyle}>
+                    <div className={myCol}><img src={require('./images/eye.svg')} style={myStyleDiv} /></div>
+                    <div className={myCol}><img src={require('./images/nurse.svg')} style={myStyleDiv} /></div>
+                    <div className={myCol}><img src={require('./images/bed.svg')} style={myStyleDiv} /></div>
+		   <div className={myCol}><img src={require('./images/heart.svg')} style={myStyleDiv} /></div>
+
+
+		</div>
+            </ResponsiveContainer>
     );
   }
 }
